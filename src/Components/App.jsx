f80be0cc1c5638a4d6d618abb840ee78b76d7827
Níkolas Vargas <vargasnikolass@gmail.com>
2018-10-18 17:07:33 -0300

    hero-head container removed

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -8,56 +8,54 @@ import '../styles/app.css';
 export default props => (
   <div>
     <section className="hero is-white is-fullheight has-text-centered">
-      <div className="hero-head">
-      </div>
       <div className="hero-body">
         <div className="container">
-              <h1 className="title is-spaced is-size-1-desktop is-size-2-tablet is-size-3-mobile">Full-Stack Developer</h1>
-              <h2 className="subtitle is-size-4-desktop">I build mobile apps, software and websites</h2>
-              <img className="avatar" width="220px" src={require('../images/welder.svg')} />
-              <h2 className="subtitle is-size-4-desktop">Porto Alegre, RS | Brasil</h2>
+          <h1 className="title is-spaced is-size-1-desktop is-size-2-tablet is-size-3-mobile">Full-Stack Developer</h1>
+          <h2 className="subtitle is-size-4-desktop">I build mobile apps, software and websites</h2>
+          <img className="avatar" width="220px" src={require('../images/welder.svg')} />
+          <h2 className="subtitle is-size-4-desktop">Porto Alegre, RS | Brasil</h2>
         </div>
       </div>
       <div className="hero-foot">
-      <nav className="tabs is-boxed is-fullwidth">
-        <div className="container">
-          <ul className="is-flex">
-            <li>
-              <a href="https://github.com/nikolasvargas" className="button is-large">
-                <span className="icon">
-                  <FontAwesomeIcon icon={faGithub} />
-                </span>
-                <span>Github</span>
-              </a>
-            </li>
-            <li>
-              <a href="https://twitter.com/_nikolasm" className="button is-large">
-                <span className="icon">
-                  <FontAwesomeIcon icon={faTwitter} />
-                </span>
-                <span>Twitter</span>
-              </a>
-            </li>
-            <li>
-              <a href="https://www.linkedin.com/in/nikolasvargas" className="button is-large">
-                <span className="icon">
-                  <FontAwesomeIcon icon={faLinkedin} />
-                </span>
-                <span>Linkedin</span>
-              </a>
-            </li>
-            <li>
-              <a href="mailto:nikolas@nikolas.pw" className="button is-large">
-                <span className="icon">
-                  <FontAwesomeIcon icon={faEnvelope} />
-                </span>
-                <span>Contact me</span>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </div>
+        <nav className="tabs is-boxed is-fullwidth">
+          <div className="container">
+            <ul className="is-flex">
+              <li>
+                <a href="https://github.com/nikolasvargas" className="button is-large">
+                  <span className="icon">
+                    <FontAwesomeIcon icon={faGithub} />
+                  </span>
+                  <span>Github</span>
+                </a>
+              </li>
+              <li>
+                <a href="https://twitter.com/_nikolasm" className="button is-large">
+                  <span className="icon">
+                    <FontAwesomeIcon icon={faTwitter} />
+                  </span>
+                  <span>Twitter</span>
+                </a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/nikolasvargas" className="button is-large">
+                  <span className="icon">
+                    <FontAwesomeIcon icon={faLinkedin} />
+                  </span>
+                  <span>Linkedin</span>
+                </a>
+              </li>
+              <li>
+                <a href="mailto:nikolas@nikolas.pw" className="button is-large">
+                  <span className="icon">
+                    <FontAwesomeIcon icon={faEnvelope} />
+                  </span>
+                  <span>Contact me</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
     </section>
   </div>
 )
