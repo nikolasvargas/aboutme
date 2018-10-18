5f3be1a29db53d8c2c3939f03e1988395a236288
Níkolas Vargas <vargasnikolass@gmail.com>
2018-10-18 02:04:50 -0300

    webpack troll

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -3,20 +3,22 @@ import 'modules/bulma/css/bulma.min.css';
 import 'modules/font-awesome/css/font-awesome.min.css';
 
 class App extends Component {
-    render() {
-        return (
-            <section className="section">
-                <div className="container">
-                    <h1 className="title">
-                        Níkolas Vargas
-                    </h1>
-                    <p className="subtitle">
-                        Software <strong>Developer</strong>
-                    </p>
-                </div>
-            </section>
-        )
-    }
+  render() {
+    return (
+      <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+        <section className="hero is-fullheight has-background-black">
+          <div className="hero-body">
+            <div className="container">
+            </div>
+          </div>
+        </section>
+        <section className="section">
+          <div className="container">
+          </div>
+        </section>
+      </div>
+    )
+  }
 }
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -5,74 +5,73 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = (env, argv) => {
-    let plugins = [new MiniCssExtractPlugin({ filename: 'bundle.css' })]
+  let plugins = [
+    new MiniCssExtractPlugin({
+      filename: 'bundle.css'
+    })
+  ]
 
-    if (argv.mode === 'production') {
-        plugins.push(
-            new HtmlWebpackPlugin({
-                hash: true,
-                minify: {
-                    html5: true,
-                    collapseWhitespace: true,
-                    removeComments: true
-                },
-                filename: 'index.html',
-                template: path.resolve(__dirname, 'src/index.html')
-            })
-        );
-        plugins.push(new UglifyJSPlugin({ extractComments: true }));
-        plugins.push(new webpack.optimize.ModuleConcatenationPlugin());
-    } else {
-        plugins.push(
-            new HtmlWebpackPlugin({
-                filename: 'index.html',
-                template: path.resolve(__dirname, 'src/index.html')
-            })
-        )
-    }
+  if (argv.mode === 'production') {
+    plugins.push(
+      new HtmlWebpackPlugin({
+        hash: true,
+        minify: {
+          html5: true,
+          collapseWhitespace: true,
+          removeComments: true
+        },
+        filename: 'index.html',
+        template: path.resolve(__dirname, 'src/index.html')
+      })
+    );
+    plugins.push(new UglifyJSPlugin({ extractComments: true }));
+  }
 
-    return {
-        entry: path.resolve(__dirname, 'src/index.jsx'),
-        output: {
-            path: path.resolve(__dirname, 'public'),
-            filename: 'bundle.js'
+  return {
+    entry: path.resolve(__dirname, 'src/index.jsx'),
+    output: {
+      path: path.resolve(__dirname, 'public/'),
+      filename: 'bundle.js'
+    },
+    devtool: 'inline-source-map',
+    watchOptions: {
+      poll: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
         },
-        devtool: 'inline-source-map',
-        module: {
-            rules: [
-                {
-                    test: /\.(js|jsx)$/,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: 'babel-loader'
-                    }
-                },
-                {
-                    test: /\.css$/,
-                    use: [
-                        argv.mode === 'production' ?
-                            MiniCssExtractPlugin.loader : "style-loader", "css-loader"
-                    ]
-                },
-                {
-                    test: /\.(jpe|jpg|woff|woff2|eot|ttf|svg)(\?.*$|$)/,
-                    use: {
-                        loader: 'file-loader',
-                        options: {}
-                    }
-                }
-            ]
+        {
+          test: /\.css$/,
+          use: [
+              MiniCssExtractPlugin.loader,
+              "css-loader"
+          ]
         },
-        plugins,
-        resolve: {
-            extensions: ['.js', '.jsx'],
-            alias: {
-                modules: path.resolve(__dirname, 'node_modules')
-            }
-        },
-        devServer: {
-            contentBase: path.join(__dirname, 'public'),
-            port: 3333
+        {
+          test: /\.(jpe|jpg|woff|woff2|eot|ttf|svg)(\?.*$|$)/,
+          use: {
+            loader: 'file-loader',
+            options: {}
+          }
         }
+      ]
+    },
+    plugins,
+    resolve: {
+      extensions: ['.js', '.jsx'],
+      alias: {
+        modules: path.resolve(__dirname, 'node_modules')
+      }
+    },
+    devServer: {
+      contentBase: path.join(__dirname, 'public/'),
+      port: 3333
     }
+  }
 }
