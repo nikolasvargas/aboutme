3f3d65269cf93f45a0b2345973cff2d6356f33b1
Níkolas Vargas <vargasnikolass@gmail.com>
2018-10-17 22:04:05 -0300

    ready to develop

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,78 @@
-const webpack = require('webpack');
-const path = require('path');
-const cssExtractPlugin = require('mini-css-extract-plugin');
+const webpack              = require('webpack');
+const path                 = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin    = require('html-webpack-plugin');
+const UglifyJSPlugin       = require('uglifyjs-webpack-plugin');
 
-module.exports = {
-    entry: path.resolve(__dirname, 'src/index.jsx'),
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
+module.exports = (env, argv) => {
+    let plugins = [ new MiniCssExtractPlugin({ filename: 'bundle.css' }) ]
+
+    if (argv.mode === 'production') {
+        plugins.push(
+            new HtmlWebpackPlugin({
+                hash: true,
+                minify: {
+                    html5: true,
+                    collapseWhitespace: true,
+                    removeComments: true
+                },
+                filename: 'index.html',
+                template: path.resolve(__dirname, 'src/index.html')
+            })
+        );
+        plugins.push(new UglifyJSPlugin({ extractComments: true }));
+        plugins.push(new webpack.optimize.ModuleConcatenationPlugin());
+    } else {
+        plugins.push(
+            new HtmlWebpackPlugin({
+                filename: 'index.html',
+                template: path.resolve(__dirname, 'src/index.html')
+            })
+        )
+    }
+
+    return {
+        entry: path.resolve(__dirname, 'src/index.jsx'),
+        output: {
+            path: path.resolve(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        devtool: 'inline-source-map',
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader'
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        argv.mode === 'production' ?
+                            MiniCssExtractPlugin.loader : "style-loader", "css-loader"
+                    ]
+                },
+                {
+                    test: /\.(woff|woff2|ttf|eot|svg*.*)$/,
+                    use: {
+                      loader: 'file-loader',
+                      options: {}
+                    }
                 }
-            },
-            {
-                test: /\.css$/,
-                use: [ cssExtractPlugin.loader, "css-loader" ]
-            },
-            {
-                test: /\.(woff|woff2|ttf|eot|svg*.*)$/,
-                use: {
-                  loader: 'file-loader',
-                  options: {}
-                }
+            ]
+        },
+        plugins,
+        resolve: {
+            extensions: [ '.js', '.jsx' ],
+            alias: {
+                modules: path.resolve(__dirname, 'node_modules')
             }
-        ]
-    },
-    plugins: [ new cssExtractPlugin({ filename: 'bundle.css' }) ],
-    resolve: {
-        extensions: [ '.js', '.jsx' ],
-        alias: {
-            modules: path.resolve(__dirname, 'node_modules')
+        },
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            port: 3333
         }
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        port: 3333
     }
 }
